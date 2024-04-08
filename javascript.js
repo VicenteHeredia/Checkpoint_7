5ec79fec692b8f47e613bad9f09093e73437eb73
VicenteHeredia <152275830+VicenteHeredia@users.noreply.github.com>
2024-04-08 11:55:50 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,14 +1,3 @@
-/*
-Cree una función JS que acepte 4 argumentos.
-Suma los dos primeros argumentos,
-luego los dos segundos y
-multiplícalos.
-Si el número creado es mayor que 50, la consola registra
-"¡El número es mayor que 50!".
-Si es más pequeño, la consola registra 
-"¡El número es menor que 50!"
-*/
-
 let operations_one = function (a, b, c, d) {
   return (a+b);
 };
