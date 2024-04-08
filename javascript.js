ca66d9830375681759bd0c4fb042f3f0c2a0464c
VicenteHeredia <152275830+VicenteHeredia@users.noreply.github.com>
2024-04-08 17:15:23 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,29 +1,12 @@
-let operations_one = function (a, b, c, d) {
-  return (a+b);
-};
-console.log ( operations_one(1, 2, 3, 4) );
-  if (operations_one > 50) {
-  console.log('¡El número es mayor que 50!');
-  } else {
-    console.log('¡El número es menor que 50!');
-}
-
-let operations_two = function (a, b, c, d) {
-  return (c+d);
-};
-console.log ( operations_two(1, 2, 3, 4) );
-  if (operations_two > 50) {
-  console.log('¡El número es mayor que 50!');
-  } else {
-    console.log('¡El número es menor que 50!');
-}
-
 let operations_three = function (a, b, c, d) {
   return (a+b)*(c+d);
 };
-console.log ( operations_three(1, 2, 3, 4) );
-  if (operations_three > 50) {
+
+result = operations_three (5, 1, 2, 5);
+console.log(result)
+
+  if (result > 50) {
   console.log('¡El número es mayor que 50!');
   } else {
-    console.log('¡El número es menor que 50!');
+  console.log('¡El número es menor que 50!');
 }
